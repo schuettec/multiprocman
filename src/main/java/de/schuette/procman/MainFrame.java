3aa43d285314eb5f369c74662110bdd04167f730
cschu <cschu@DESKTOP-Chris.fritz.box>
2018-03-08 17:51:00 +0100

    Verwaltung offener Prozesse hinzugefügt
    - Mit Shutdown Hook

--- src/main/java/de/schuette/procman/MainFrame.java
+++ src/main/java/de/schuette/procman/MainFrame.java
@@ -293,8 +293,6 @@ public class MainFrame extends JFrame implements WindowListener, ProcessListener
 
 	@Override
 	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -311,31 +309,26 @@ public class MainFrame extends JFrame implements WindowListener, ProcessListener
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
