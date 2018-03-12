0ffc55a9d575ac6936defb2722bdc5cfd68c7ece
Christopher Schuette <christopher.schuette@remondis.de>
2018-03-12 11:28:26 +0100

    Fehlerbehebungen in ConsolePreview
    - Formatierung

--- src/main/java/com/github/schuettec/multiprocman/TextUtil.java
+++ src/main/java/com/github/schuettec/multiprocman/TextUtil.java
@@ -4,22 +4,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class TextUtil {
-  public static List<String> getLinesFromString(String s) {
-    List<String> lines = new LinkedList<>();
-    StringBuilder line = new StringBuilder("");
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      if (c == '\r') {
-        continue;
-      } else if (c == '\n') {
-        lines.add(line.toString());
-        line = new StringBuilder();
-      } else {
-        line.append(c);
-      }
-    }
-    lines.add(line.toString());
-    return lines;
-  }
+	public static List<String> getLinesFromString(String s) {
+		List<String> lines = new LinkedList<>();
+		StringBuilder line = new StringBuilder("");
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '\r') {
+				continue;
+			} else if (c == '\n') {
+				lines.add(line.toString());
+				line = new StringBuilder();
+			} else {
+				line.append(c);
+			}
+		}
+		lines.add(line.toString());
+		return lines;
+	}
 
 }
