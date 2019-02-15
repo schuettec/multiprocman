26a57efe1e979bb9219439a2af87a054476f69d7
Christopher Schuette <cschue88@googlemail.com>
2019-02-15 17:48:51 +0100

    Implement Captor Test

---
+++ src/test/java/livefilereader/captor/InputCaptorTest.java
@@ -0,0 +1,41 @@
+package livefilereader.captor;
+
+import static org.junit.Assert.assertArrayEquals;mport java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.ju
+
+import org.junit.Before;t.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InputCaptorTest {
+
+	private static final String BACKSPACE = String.valueOf((char) 0x8);
+	private static final String LINES_WITH_ANSI = "Line0\r\nLine1\n" + BACKSPACE + "1234" + BACKSPACE + BACKSPACE
+			+ BACKSPACE + BACKSPACE + "Line3\n";
+
+	@Mock
+	InputCaptorCallback callback;
+
+	@Before
+	public void setup() {
+		when(callback.shouldRun()).thenReturn(true);
+	}
+
+	@Test
+	public void shouldWriteInputToOutput() throws IOException {
+		byte[] bytes = LINES_WITH_ANSI.getBytes();
+		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		InputCaptor captor = new InputCaptor(callback, in, out);
+		captor.run();
+
+		assertArrayEquals(bytes, out.toByteArray());
+	}
+
+}
