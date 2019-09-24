63be5b95938e76b05a8987cda456c1a98256539d
Christopher Schuette <christopher.schuette@remondis.de>
2019-09-24 12:32:36 +0200

    Preferences Dialog added.

---
+++ src/main/java/com/github/schuettec/multiprocman/preferences/Preferences.java
@@ -0,0 +1,62 @@
+package com.github.schuettec.multiprocman.preferences;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Hashtable;
+import java.util.Map;
+
+import com.github.schuettec.multiprocman.ExceptionDialog;
+import com.github.schuettec.multiprocman.manager.ProcessManager;
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * This class defines the global application preferences and provides features to load/save global preferences.
+ */
+public class Preferences {
+
+	private Map<String, String> globalVariables = new Hashtable<>();
+
+	Map<String, String> getGlobalVariables() {
+		return globalVariables;
+	}
+
+	void setGlobalVariables(Map<String, String> globalVariables) {
+		this.globalVariables = globalVariables;
+	}
+
+	public static void saveToPreferences(Preferences data) {
+		try (FileOutputStream fout = new FileOutputStream(getUserFile())) {
+			XStream xstream = new XStream();
+			xstream.toXML(data, fout);
+		} catch (Exception e) {
+			ExceptionDialog.showException(ProcessManager.getInstance(), e,
+			    "Error while persisting user settings in user home directory.");
+		}
+	}
+
+	public static Preferences loadFromPreferences() {
+		File userFile = getUserFile();
+		if (userFile.exists()) {
+			try (FileInputStream fin = new FileInputStream(userFile)) {
+				XStream xstream = new XStream();
+				Object readObject = xstream.fromXML(fin);
+				if (readObject instanceof Preferences) {
+					return (Preferences) readObject;
+				}
+			} catch (Exception e) {
+				ExceptionDialog.showException(ProcessManager.getInstance(), e,
+				    "Error while loading user settings from user home directory.");
+			}
+		}
+		return new Preferences();
+	}
+
+	private static File getUserFile() {
+		return new File(System.getProperty("user.home") + File.separator + ".procman.settings");
+	}
+
+	public void save() {
+		saveToPreferences(this);
+	}
+}
---
+++ src/main/java/com/github/schuettec/multiprocman/preferences/PreferencesDialog.java
@@ -0,0 +1,244 @@
+package com.github.schuettec.multiprocman.preferences;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import com.github.schuettec.multiprocman.Resources;
+import com.github.schuettec.multiprocman.themes.ThemeUtil;
+
+public class PreferencesDialog extends JDialog {
+	private final JPanel environmentPanel = new JPanel();
+	private JTabbedPane tabbedPane;
+	private JTable tblEnv;
+	private DefaultTableModel variables;
+	private Preferences preferences;
+
+	/**
+	 * Create the dialog.
+	 *
+	 * @param parent
+	 *
+	 * @param category
+	 */
+	private PreferencesDialog(Preferences preferences, Component parent) {
+		this.preferences = preferences;
+		setIconImage(Resources.getApplicationIcon());
+		setModal(true);
+		setTitle("Preferences");
+		this.setPreferredSize(new Dimension(540, 580));
+		this.setSize(new Dimension(540, 580));
+		this.setLocationRelativeTo(parent);
+		addWindowListener(new WindowListener() {
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				performCancel();
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+			}
+		});
+
+		getContentPane().setLayout(new BorderLayout());
+
+		tabbedPane = new JTabbedPane();
+		ButtonGroup group = new ButtonGroup();
+		tabbedPane.add("Global variables", environmentPanel);
+
+		JLabel lblNewLabel_1 = new JLabel(
+		    "<html>Variables defined here can be used in launch configurations. This helps to create more portable launch configurations since path's can be configured globally for a specific system.</html>");
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GroupLayout gl_environmentPanel = new GroupLayout(environmentPanel);
+		gl_environmentPanel.setHorizontalGroup(gl_environmentPanel.createParallelGroup(Alignment.LEADING)
+		    .addGroup(gl_environmentPanel.createSequentialGroup()
+		        .addContainerGap()
+		        .addGroup(gl_environmentPanel.createParallelGroup(Alignment.LEADING)
+		            .addGroup(gl_environmentPanel.createSequentialGroup()
+		                .addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 354, Short.MAX_VALUE)
+		                .addContainerGap())
+		            .addComponent(lblNewLabel_1))));
+		gl_environmentPanel.setVerticalGroup(gl_environmentPanel.createParallelGroup(Alignment.LEADING)
+		    .addGroup(gl_environmentPanel.createSequentialGroup()
+		        .addContainerGap()
+		        .addComponent(lblNewLabel_1)
+		        .addGap(8)
+		        .addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 361, Short.MAX_VALUE)
+		        .addContainerGap()));
+
+		tblEnv = new JTable();
+		this.variables = new DefaultTableModel(new Object[][] {}, new String[] {
+		    "Variable name", "Value"
+		});
+		tblEnv.setModel(variables);
+		scrollPane_1.setViewportView(tblEnv);
+
+		JToolBar toolBar = new JToolBar();
+		toolBar.setRollover(true);
+		toolBar.setFloatable(false);
+		toolBar.setOrientation(SwingConstants.VERTICAL);
+		scrollPane_1.setRowHeaderView(toolBar);
+
+		JButton btnPlus = new JButton(new AbstractAction(null, new ImageIcon(Resources.getPlus())) {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				variables.addRow(new String[] {
+				    "", ""
+				});
+			}
+		});
+		toolBar.add(btnPlus);
+
+		JButton btnMinus = new JButton(new AbstractAction(null, new ImageIcon(Resources.getMinus())) {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int row = tblEnv.getSelectedRow();
+				if (row == -1) {
+					JOptionPane.showMessageDialog(PreferencesDialog.this, "Please select the variable to remove first.",
+					    "No selection", JOptionPane.WARNING_MESSAGE);
+				} else {
+					variables.removeRow(row);
+					if (variables.getRowCount() > 0) {
+						tblEnv.setRowSelectionInterval(variables.getRowCount() - 1, variables.getRowCount() - 1);
+					}
+				}
+			}
+		});
+		toolBar.add(btnMinus);
+		environmentPanel.setLayout(gl_environmentPanel);
+		getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton(new AbstractAction("OK") {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						performOk();
+					}
+
+				});
+				okButton.setPreferredSize(new Dimension(91, 23));
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+				getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+				    .put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "OK");
+				getRootPane().getActionMap()
+				    .put("OK", okButton.getAction());
+			}
+			{
+				JButton cancelButton = new JButton(new AbstractAction("Cancel") {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						performCancel();
+					}
+
+				});
+				cancelButton.setPreferredSize(new Dimension(91, 23));
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+				getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+				    .put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "CANCEL");
+				getRootPane().getActionMap()
+				    .put("CANCEL", cancelButton.getAction());
+			}
+		}
+
+		initializeData(preferences);
+
+		setVisible(true);
+	}
+
+	private void initializeData(Preferences preferences) {
+		preferences.getGlobalVariables()
+		    .entrySet()
+		    .stream()
+		    .forEach(entry -> this.variables.addRow(new Object[] {
+		        entry.getKey(), entry.getValue()
+				}));
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+
+	private void performCancel() {
+		dispose();
+	}
+
+	private void performOk() {
+		Map<String, String> variableMap = new Hashtable<>();
+		int rowCount = this.variables.getRowCount();
+		for (int i = 0; i < rowCount; i++) {
+			Object key = this.variables.getValueAt(i, 0);
+			Object value = this.variables.getValueAt(i, 1);
+			variableMap.put(key.toString(), value.toString());
+		}
+		preferences.setGlobalVariables(variableMap);
+
+		preferences.save();
+		dispose();
+	}
+
+	public static void main(String[] args) {
+		ThemeUtil.setLookAndFeel();
+		Preferences preferences = Preferences.loadFromPreferences();
+		new PreferencesDialog(preferences, null);
+	}
+}
