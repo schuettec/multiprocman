bd0da91530858d112f7afea738fa1dc6191ad819
Christopher Schuette <christopher.schuette@remondis.de>
2019-10-11 13:56:41 +0200

    Build Fix

--- src/main/java/com/github/schuettec/multiprocman/Appendable.java
+++ src/main/java/com/github/schuettec/multiprocman/Appendable.java
@@ -4,26 +4,26 @@ import java.awt.Color;
 
 public interface Appendable {
 
-    /**
-     * Appends the specified string in the specified color.
-     *
-     * @param c
-     *            The string to append
-     * @param s
-     *            The color to use.
-     * @param supportBackspace
-     *            If <code>true</code> the ASCII backspace code is recognized to provide rudimental support for layouted
-     *            appends.
-     */
-    public void append(Color c, String s, boolean isSupportAsciiCodes);
+	/**
+	 * Appends the specified string in the specified color.
+	 *
+	 * @param c
+	 *        The string to append
+	 * @param s
+	 *        The color to use.
+	 * @param isSupportAsciiCodes
+	 *        If <code>true</code> the ASCII backspace code is recognized to provide rudimental support for layouted
+	 *        appends.
+	 */
+	public void append(Color c, String s, boolean isSupportAsciiCodes);
 
-    /**
-     * @param s
-     *            Appends this string with support for ANSI color codes.
-     * @param supportBackspace
-     *            If <code>true</code> the ASCII backspace code is recognized to provide rudimental support for layouted
-     *            appends.
-     */
-    public void appendANSI(String s, boolean isSupportAsciiCodes);
+	/**
+	 * @param s
+	 *        Appends this string with support for ANSI color codes.
+	 * @param isSupportAsciiCodes
+	 *        If <code>true</code> the ASCII backspace code is recognized to provide rudimental support for layouted
+	 *        appends.
+	 */
+	public void appendANSI(String s, boolean isSupportAsciiCodes);
 
 }
